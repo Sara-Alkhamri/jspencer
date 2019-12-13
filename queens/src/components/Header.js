2f0c3940fc1d0730f2636208bbfff7368c42ca97
Sara Alkhamri <s.alkhamri@gmail.com>
2019-12-13 13:36:02 -0800

    Added basic logo to header component and created assets folder for photos

---
+++ queens/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../assets/eye.png'
+
+function Header() {
+    return (
+        <div className="header">
+            <img 
+            className="eye-logo"
+            alt="logo"
+            src={logo}
+            width="100"
+            height="50"
+            />
+        </div>
+    )
+}
+
+export default Header;
--- queens/src/components/Home.js
+++ queens/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header'
 
 function Home() {
     return (
         <div className="home">
            <p>Home </p> 
+           <Header />
         </div>
     )
 }
