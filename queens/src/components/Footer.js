107bc7fdbb5ecbf91633a519f3da858d3030976f
Sara Alkhamri <s.alkhamri@gmail.com>
2019-12-15 19:58:10 -0800

    Imported Nav component into Footer

---
+++ queens/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../assets/eye'
+import Nav from '../components/Nav'
+
+function Footer() {
+    return (
+       <div className="footer">
+            <Nav />
+        </div>
+    )
+}
