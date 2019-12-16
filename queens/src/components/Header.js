a4532c0c5c4ae0164d8d01714eeff0d485e4e427
Sara Alkhamri <s.alkhamri@gmail.com>
2019-12-15 19:46:36 -0800

    Added more info to Home.js

--- queens/src/components/Header.js
+++ queens/src/components/Header.js
@@ -13,8 +13,6 @@ function Header() {
             height="50"
             />
             <Nav />
-            <h1>Ascended Queens</h1>
-            <p>Tune into these 2 dope queens as they discuss spirituality, love, relationships, being queer, life & of course ascension.</p>
         </div>
     )
 }
--- queens/src/components/Home.js
+++ queens/src/components/Home.js
@@ -6,6 +6,8 @@ const Home = () => {
     return (
         <div className="home">
            <Header />
+           <h1>Ascended Queens</h1>
+            <p>Tune into these 2 dope queens as they discuss spirituality, love, relationships, being queer, life & of course ascension.</p>
         </div>
     )
 }
