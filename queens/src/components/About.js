8cc44c5903ffcd80adfbdeaca22bb6f47fca9e2e
Sara Alkhamri <s.alkhamri@gmail.com>
2019-12-15 19:29:45 -0800

    Added bio to jessica in abot.js

--- queens/src/components/About.js
+++ queens/src/components/About.js
@@ -9,7 +9,11 @@ function About() {
                     className="jessica"
                     alt="jessica"
                     src={jessica}
+                    
                 />
+                <p> Jessica Spencer is an Author, Radio host at KPFK 90.7 Los angeles. 
+                    thiswayout.org co-anchor, Realtor and and an AKA
+                </p>
             </div>
 
             <div className="about-germaine">
