8896ff145bf7915f1e766c7097d7a203f14f3e97
Sara Alkhamri <s.alkhamri@gmail.com>
2019-12-19 14:01:16 -0800

    Installed Formik and Yup for contact component

--- queens/src/components/Contact.js
+++ queens/src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Contact(props) {
-    return <h4>Contact Page</h4>
+    return <h4>We'd love to hear from you! Please share your thoughts using the form below.</h4>
 }
 
 export default Contact;
