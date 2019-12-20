50b0329b652db0f4735c4135a812277bf71907ac
Sara Alkhamri <s.alkhamri@gmail.com>
2019-12-20 13:29:48 -0800

    Added gemotery.jpg to assets and imported it into home.js

--- queens/src/components/Home.js
+++ queens/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import geometry from '../assets/geometry.jpg'
 
 
 const Home = () => {
@@ -6,7 +7,15 @@ const Home = () => {
         <div className="home">
           
            <h1>Ascended Queens</h1>
-            <p>Tune into these 2 dope queens as they discuss spirituality, love, relationships, being queer, life & of course ascension.</p>
+           <img 
+            className="geometry"
+            alt="geometry"
+            src={geometry}
+            width="300"
+            height="300"
+            />
+            <p>Tune into these 2 dope queens as they discuss spirituality, love, relationships, being queer, 
+                life & of course ascension.</p>
           
         </div>
     )
