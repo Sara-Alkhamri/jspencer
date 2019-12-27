5bbd4cf3792c6efbafd0d7540ff1f5ce51883263
Sara Alkhamri <s.alkhamri@gmail.com>
2019-12-26 16:10:13 -0800

    Deleted germaine infofrom about.js

--- queens/src/components/About.js
+++ queens/src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import jessica from '../assets/jessica.jpg'
-import germaine from '../assets/germaine.jpeg'
+
 function About() {
     return  (
         <div className="about-container">
@@ -14,16 +14,6 @@ function About() {
                 <p> Jessica Spencer is an Author, Radio host at KPFK 90.7 Los angeles. 
                     thiswayout.org co-anchor, Realtor and and an AKA.
                 </p>
-            </div>
-
-            <div className="about-germaine">
-                <img
-                    className="germaine"
-                    alt="germaine"
-                    src={germaine}
-                />
-                <p> Germaine Nichols was born and raised in Long Beach, California.  
-                    Her upbringing in this vibrant, eclectic community laid the foundation for her development as a positive minded artist and consciously connected woman.</p>
             </div>
         </div>
     )
