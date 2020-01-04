584924281588a629e88bac991e9bf316c5e248c4
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-03 16:03:23 -0800

    Pushing unsaved changes

--- queens/src/components/realty.js
+++ queens/src/components/realty.js
@@ -7,15 +7,15 @@ function Realty() {
         <h2>Licensed Realtor at Keller Williams Realty, Inc.</h2>
         <p>Greator Los Angeles area</p>
         <p>Some of the homes I've sold: </p>
-         {/* add carousel here */}
+        
          <Carousel>
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
-      </Carousel>
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+            <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+        </Carousel>
 
 
     </div>
