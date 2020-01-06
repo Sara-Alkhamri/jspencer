cdd00f787b622f425938387a653128147b08e7cc
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-06 11:56:04 -0800

    Replaced main photo at Home.js with jess_less.jpg

--- queens/src/components/Home.js
+++ queens/src/components/Home.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import geometry from '../assets/geometry.jpg'
+import jess_les from '../assets/jess_les.jpg'
 
 
 const Home = () => {
     return (
         <div className="home">
-          
            <h1>Jessica Spencer</h1>
            <img 
-            className="geometry"
-            alt="geometry"
-            src={geometry}
-            width="300"
+            className="home-jess-les"
+            alt="jess-les"
+            src={jess_les}
+            width="350"
             height="300"
             />
             <p>Author, Radio host at KPFK 90.7 Los angeles. 
             thiswayout.org co-anchor, and Realtor.</p>
-          
         </div>
     )
 }
