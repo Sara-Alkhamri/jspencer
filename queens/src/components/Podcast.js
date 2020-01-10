88dd9a9010686d77f86c4e83fbff7478a17c4d43
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-09 18:43:41 -0800

    Added title attribute to iframe tags in Podcast.js

--- queens/src/components/Podcast.js
+++ queens/src/components/Podcast.js
@@ -6,19 +6,19 @@ function Podcast() {
     return (
     
         <div>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/3fHT2V8wRnDMQYVWIqmc8H" width="100%" height="232" 
+            <iframe title="intro" src="https://open.spotify.com/embed-podcast/episode/3fHT2V8wRnDMQYVWIqmc8H" width="100%" height="232" 
             frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
-            <iframe src="https://open.spotify.com/embed-podcast/episode/5DWPDsxQrxbVDrosKOAvmI" width="100%" height="232" 
+            <iframe title="epi1" src="https://open.spotify.com/embed-podcast/episode/5DWPDsxQrxbVDrosKOAvmI" width="100%" height="232" 
             frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
-            <iframe src="https://open.spotify.com/embed-podcast/episode/0s9qmHvW4oUnm2YOaVWzo4" width="100%" height="232" 
+            <iframe title="epi2" src="https://open.spotify.com/embed-podcast/episode/0s9qmHvW4oUnm2YOaVWzo4" width="100%" height="232" 
             frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
-            <iframe src="https://open.spotify.com/embed-podcast/episode/4qg2iMnGQa1wG2PFoIoiJX" width="100%" height="232" 
+            <iframe title="epi3" src="https://open.spotify.com/embed-podcast/episode/4qg2iMnGQa1wG2PFoIoiJX" width="100%" height="232" 
             frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
-            <iframe src="https://open.spotify.com/embed-podcast/episode/1LcTUhn5wI5H5jATfdao5S" width="100%" height="232" 
+            <iframe title="epi4" src="https://open.spotify.com/embed-podcast/episode/1LcTUhn5wI5H5jATfdao5S" width="100%" height="232" 
             frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         </div>
     )
