223bc65a0e9c10ed54382e38b93b296e47028ed8
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-13 19:46:41 -0800

    Cleaned up Podvcast.js

--- queens/src/components/Podcast.js
+++ queens/src/components/Podcast.js
@@ -9,28 +9,26 @@ function Podcast() {
             <div className="queens">
                 <h3>Ascended Queens</h3>
                 <iframe title="intro" src="https://open.spotify.com/embed-podcast/episode/3fHT2V8wRnDMQYVWIqmc8H" width="50%" height="232" 
-                frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
+                frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
 
                 <iframe title="epi1" src="https://open.spotify.com/embed-podcast/episode/5DWPDsxQrxbVDrosKOAvmI" width="50%" height="232" 
-                frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
+                frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
 
                 <iframe title="epi2" src="https://open.spotify.com/embed-podcast/episode/0s9qmHvW4oUnm2YOaVWzo4" width="50%" height="232" 
-                frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
+                frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
 
                 <iframe title="epi3" src="https://open.spotify.com/embed-podcast/episode/4qg2iMnGQa1wG2PFoIoiJX" width="50%" height="232" 
-                frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
+                frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
 
                 <iframe title="epi4" src="https://open.spotify.com/embed-podcast/episode/1LcTUhn5wI5H5jATfdao5S" width="50%" height="232" 
-                frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
+                frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
             </div>
         
         <div className="jewel">
           {/* IG vids here */}
         </div>    
 
-        </div>
-     
-        
+    </div> 
     )
 }
 
