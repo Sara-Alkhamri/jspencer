c510df4769b26c018ceb9c3d29f09263f16f97c3
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-16 14:23:49 -0800

    Added germaine,jpeg to assets to include in podcast description

--- queens/src/components/Podcast.js
+++ queens/src/components/Podcast.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+
 function Podcast() {
    
     return (
@@ -8,6 +9,13 @@ function Podcast() {
         
             <div className="queens">
                 <h3>Ascended Queens</h3>
+                {/* <img 
+                    className="ascended-queens"
+                    alt="ascended-queens"
+                    src={ascended_queens}
+                    width="350"
+                    height="300"
+                    /> */}
                 <iframe title="intro" src="https://open.spotify.com/embed-podcast/episode/3fHT2V8wRnDMQYVWIqmc8H" width="50%" height="232" 
                 frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
             </div>
