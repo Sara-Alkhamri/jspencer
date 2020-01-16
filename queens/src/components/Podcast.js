e240b573a061f26c394ba62a1bf8f2e980190155
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-16 14:31:52 -0800

    Added jess_bday.jpg to podcast.js

--- queens/src/components/Podcast.js
+++ queens/src/components/Podcast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import jess_bday from '../assets/jess_bday.jpg'
 
 
 
@@ -9,13 +10,13 @@ function Podcast() {
         
             <div className="queens">
                 <h3>Ascended Queens</h3>
-                {/* <img 
-                    className="ascended-queens"
-                    alt="ascended-queens"
-                    src={ascended_queens}
+                <img 
+                    className="jess-bday"
+                    alt="jess-bday"
+                    src={jess_bday}
                     width="350"
                     height="300"
-                    /> */}
+                    />
                 <iframe title="intro" src="https://open.spotify.com/embed-podcast/episode/3fHT2V8wRnDMQYVWIqmc8H" width="50%" height="232" 
                 frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
             </div>
