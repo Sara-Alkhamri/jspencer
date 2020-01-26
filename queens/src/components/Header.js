f0c4e8c4b9986a1383ddbc59f9cc0c27dd12a5dd
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-25 20:17:06 -0800

    Renamed eye.png to favicon.ico, and commented eye.png in Header.js component

--- queens/src/components/Header.js
+++ queens/src/components/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from '../assets/eye.png'
+// import logo from '../assets/eye.png'
 
 function Header() {
     return (
         <div className="header">
-            <img 
+            {/* <img 
             className="eye-logo"
             alt="logo"
             src={logo}
             width="100"
             height="50"
-            />
+            /> */}
         </div>
     )
 }
