38e5518ac1aa2012042cff0b88738328dbaf3c07
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-26 15:10:14 -0800

    Added linkedin icon to Footer.js and basic styling in App.css

--- queens/src/components/Footer.js
+++ queens/src/components/Footer.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import instagram from '../assets/instagram.png'
+import linkedin from '../assets/linkedin.png'
 
 function Footer() {
     return (
        <div className="footer">
         <Nav />
-       <div className="footer-icons">
-            <a href="https://www.instagram.com/ascendedqueens/">
-                            <img 
-                                className="insta-btn" 
-                                alt="insta-icon"
-                                src={instagram}
-                                width="50"
-                                height="50"
-                            />
-                        </a>
+        <div className="footer-icons">
+                <a href="https://www.instagram.com/ascendedqueens/">
+                        <img 
+                            className="insta-btn" 
+                            alt="insta-icon"
+                            src={instagram}
+                            width="50"
+                            height="50"
+                        />
+                </a>
+
+                <a href="https://www.linkedin.com/in/jessica-spencer-43187148/">
+                    <img 
+                        className="linkedin-btn"
+                        alt="linkedin"
+                        src={linkedin}
+                        width="50"
+                        height="50"
+                    />
+                </a>
             </div>
         </div>
 
