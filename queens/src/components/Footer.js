73763db092c2ad8fd0f35419219d7052079454f7
Sara Alkhamri <s.alkhamri@gmail.com>
2020-01-26 14:58:20 -0800

    Added instagram icon to Footer.js

--- queens/src/components/Footer.js
+++ queens/src/components/Footer.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import instagram from '../assets/instagram.png'
 
 function Footer() {
     return (
        <div className="footer">
-            <Nav />
+        <Nav />
+       <div className="footer-icons">
+            <a href="https://www.instagram.com/ascendedqueens/">
+                            <img 
+                                className="insta-btn" 
+                                alt="insta-icon"
+                                src={instagram}
+                                width="50"
+                                height="50"
+                            />
+                        </a>
+            </div>
         </div>
+
+        
     )
 }
 
