06c1a9ae00b7444fe1fe060929127a673cd4468a
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-03 20:10:45 -0800

    Deleted logo image on Header.js

--- queens/src/components/Header.js
+++ queens/src/components/Header.js
@@ -4,13 +4,6 @@ import React from 'react';
 function Header() {
     return (
         <div className="header">
-            {/* <img 
-            className="eye-logo"
-            alt="logo"
-            src={logo}
-            width="100"
-            height="50"
-            /> */}
         </div>
     )
 }
