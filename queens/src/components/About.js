ac71ea28d171d236d7da17201389ae29e68599fa
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-12 15:34:24 -0800

    Refactored podcast and about components to include new design

--- queens/src/components/About.js
+++ queens/src/components/About.js
@@ -3,17 +3,39 @@ import jessica from '../assets/jessica.jpg'
 
 function About() {
     return  (
-        <div className="about-container">
-            <div className="about-jessica">
+        <div className="site-section" id="about-section">
+            <div className="container">
+            <div class="row align-items-center">
+            <div class="col-lg-6 mb-5">
                 <img
-                    className="jessica"
+                    className="img-fluid"
                     alt="jessica"
                     src={jessica}
                     
                 />
-                <p> Jessica Spencer is an Author, Radio host at KPFK 90.7 Los angeles. 
+                </div>
+
+            <div class="col-lg-5 ml-auto section-title">    
+            <h2 class="title text-primary mb-3">About me</h2>
+                <p className="mb-4"> Jessica Spencer is an Author, Radio host at KPFK 90.7 Los angeles. 
                     thiswayout.org co-anchor, Realtor in the greator Los Angeles area.
                 </p>
+                <div class="d-flex">
+                <ul class="list-unstyled ul-check success mr-5">
+                <li>Design</li>
+                <li>Development</li>
+                <li>eCommerce</li>
+                
+              </ul>
+              <ul class="list-unstyled ul-check success">
+                
+                <li>Mobile Apps</li>
+                <li>Copywriting</li>
+                <li>SEO</li>
+              </ul>
+                    </div>    
+                    </div>
+                </div>
             </div>
         </div>
     )
--- queens/src/components/Podcast.js
+++ queens/src/components/Podcast.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import jess_bday from '../assets/jess_bday.jpg';
-import germaine from '../assets/germaine.jpeg';
-import instagram from '../assets/instagram.png';
+// import germaine from '../assets/germaine.jpeg';
+// import instagram from '../assets/instagram.png';
 import sacred_geometry from '../assets/sacred_geometry.png'
 import jessica_2013 from '../assets/jessica_2013.jpeg'
 
@@ -10,73 +10,82 @@ import jessica_2013 from '../assets/jessica_2013.jpeg'
 function Podcast() {
    
     return (
-        <div className="podcasts">
+        <div className="site-section" id="portfolio-section">
+            <div className="container">
+                <div className="row mb-5 ">
+                    <div className="col-md-7 section-title text-center mx-auto">
+                        <span className="sub-title mb-2 d-block">Recent Podcasts</span>
+                        <h2 className="title text-primary mb-3">I love sharing my thoughts with you, check out some of my latest podcasts</h2>
+                    </div>
+                </div>
         
-            <div className="queens">
-                <h3>Ascended Queens</h3>
-                <img 
-                    className="jess-bday"
-                    alt="jess-bday"
-                    src={jess_bday}
-                    width="350"
-                    height="350"
-                    />
-
+            <div className="row mb-5">
+                <div className="col-lg-6 mb-4 mb-lg-0">
                     <img 
-                    className="germaine"
+                        className="img-fluid"
+                        alt="jess-bday"
+                        src={jess_bday}
+                        />
+                    {/* <img 
+                    className="img-fluid"
                     alt="germaine"
                     src={germaine}
-                    width="350"
-                    height="350"
-                    />
+                    /> */}
+                </div>
+                <div class="col-lg-5 h-100 jm-sticky-top ml-auto">
+                    <h3>Acsended Queens</h3>
 
-                    <p>Tune into these dope queens as they discuss spirituality, love, relationships, being queer, life and of course ascension.</p>
+                    <p className="mb-4">Tune into these dope queens as they discuss spirituality, love, relationships, being queer, life and of course ascension.</p>
                     
-                    <p>
-                    Follow us on instagram
-                        <a href="https://www.instagram.com/ascendedqueens/">
-                            <img 
+                    <p className="mb-4">
+                        <a href="https://www.instagram.com/ascendedqueens/" className="readmore">Follow us on instagram
+                            {/* <img 
                                 className="insta-btn" 
                                 alt="insta-icon"
                                 src={instagram}
-                                width="50"
+                                width="60"
                                 height="50"
-                            />
+                            /> */}
                         </a>
                     </p>
 
                 <iframe title="intro" src="https://open.spotify.com/embed-podcast/episode/3fHT2V8wRnDMQYVWIqmc8H" width="50%" height="232" 
                 frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe><br></br>
+                </div>
             </div>
-        
-        <div className="jewel">
-        <h3>Jewel Drop Wednesday's</h3> 
-          <p>Every Wednesday, I drop some spirituality jewels on you via instagram.</p>
-          <a href="https://www.instagram.com/p/B2kyFVogY-c/">
+        <div className="row mb-5">
+          <div class="col-lg-6 order-1 order-lg-2 mb-4 mb-lg-0">
             <img 
-                className="jewel-img"
+                className="img-fluid"
                 alt="jewel-img"
                 src={sacred_geometry}
                 width="350"
-                height="350"
+                height="300"
             />
-          </a>
-        </div> 
-
-        <div className="ascended-radio">
-        <h3>Ascended Radio</h3> 
-            <p>This show will provide intriguing and life transforming wisdom regarding the process of ascension or spiritual awakening in a simple and easily understood manner.</p>
-            <a href="https://www.blogtalkradio.com/ascended-radio">
-            <img 
-                className="jessica_2013"
-                alt="jessica_2013"
-                src={jessica_2013}
-                width="350"
-                height="350"
-            />
-            </a>
-        </div>   
-
+          </div>
+            <div className="col-lg-5 h-100 jm-sticky-top mr-auto order-2 order-lg-1">  
+                <h3>Jewel Drop Wednesday's</h3> 
+                <p className="mb-4">Every Wednesday, I drop some spirituality jewels on you via instagram.</p>
+                <p className="mb-4"><a href="https://www.instagram.com/p/B2kyFVogY-c/" className="readmore">Listen Here</a></p>
+            </div> 
+       </div>                     
+        <div className="row mb-5">
+            <div class="col-lg-6 mb-4 mb-lg-0">
+                <img 
+                    className="img-fluid"
+                    alt="jessica_2013"
+                    src={jessica_2013}
+                    width="350"
+                    height="350"
+                />
+            </div>
+            <div className="col-lg-5 h-100 jm-sticky-top ml-auto">
+                <h3>Ascended Radio</h3> 
+                <p className="mb-4">This show will provide intriguing and life transforming wisdom regarding the process of ascension or spiritual awakening in a simple and easily understood manner.</p>
+                <p className="mb-4"><a href="https://www.blogtalkradio.com/ascended-radio" className="readmore">Listen Here</a></p>
+                </div>   
+            </div>
+        </div>
     </div> 
     )
 }
