f813c3736902b2227b07ae9646319fff9920347a
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-13 15:53:49 -0800

    Fixed css classes in App

--- queens/src/App.js
+++ queens/src/App.js
@@ -14,13 +14,13 @@ function App() {
   return (
     <div className="site-wrap">
       {/* site-mobile-menu */}
-    <div class="site-mobile-menu site-navbar-target">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
+    <div className="site-mobile-menu site-navbar-target">
+      <div className="site-mobile-menu-header">
+        <div className="site-mobile-menu-close mt-3">
+          <span className="icon-close2 js-menu-toggle"></span>
         </div>
       </div>
-      <div class="site-mobile-menu-body"></div>
+      <div className="site-mobile-menu-body"></div>
     </div> 
       <Nav />
       <Header />
