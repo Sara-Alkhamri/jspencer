142f08ce3de35b5e3e8178ace080aa3735b1f746
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-20 20:18:03 -0800

    More typos fixed in Footer.js

--- queens/src/components/Footer.js
+++ queens/src/components/Footer.js
@@ -3,14 +3,14 @@ import React from 'react'
 function Footer() {
     return (
        <div className="site-footer">
-            <div class="container">
+            <div className="container">
             <div className="row pt-5 mt-5 text-center">
                 <div className="col-md-12">
                 <div className="mb-4">
                   
                   <a href="https://twitter.com/Thejspencer" className="pl-3 pr-3"><span className="icon-twitter"></span></a>
                   <a href="https://www.instagram.com/jaspencer08/" className="pl-3 pr-3"><span className="icon-instagram"></span></a>
-                  <a href="https://www.linkedin.com/in/jessica-spencer-43187148/" class="pl-3 pr-3"><span className="icon-linkedin"></span></a>
+                  <a href="https://www.linkedin.com/in/jessica-spencer-43187148/" className="pl-3 pr-3"><span className="icon-linkedin"></span></a>
                 </div>
                 </div>
             </div>
