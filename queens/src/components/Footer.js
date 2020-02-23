5a5681e24d37bdda71570e9b7d7b2f9c330d597a
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-22 23:05:06 -0800

    Fixed indenting in Footer.js

--- queens/src/components/Footer.js
+++ queens/src/components/Footer.js
@@ -4,17 +4,15 @@ function Footer() {
     return (
        <div className="site-footer">
             <div className="container">
-            <div className="row pt-5 mt-5 text-center">
-                <div className="col-md-12">
-                <div className="mb-4">
-                  
-                  <a href="https://twitter.com/Thejspencer" className="pl-3 pr-3"><span className="icon-twitter"></span></a>
-                  <a href="https://www.instagram.com/jaspencer08/" className="pl-3 pr-3"><span className="icon-instagram"></span></a>
-                  <a href="https://www.linkedin.com/in/jessica-spencer-43187148/" className="pl-3 pr-3"><span className="icon-linkedin"></span></a>
+                <div className="row pt-5 mt-5 text-center">
+                    <div className="col-md-12">
+                        <div className="mb-4">
+                            <a href="https://twitter.com/Thejspencer" className="pl-3 pr-3"><span className="icon-twitter"></span></a>
+                            <a href="https://www.instagram.com/jaspencer08/" className="pl-3 pr-3"><span className="icon-instagram"></span></a>
+                            <a href="https://www.linkedin.com/in/jessica-spencer-43187148/" className="pl-3 pr-3"><span className="icon-linkedin"></span></a>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-
             </div>
         </div>
 
