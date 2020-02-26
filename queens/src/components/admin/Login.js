5446de9000db0ec248250666aba0037243b8a996
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-25 17:05:14 -0800

    Login tested and completed

--- queens/src/components/admin/Login.js
+++ queens/src/components/admin/Login.js
@@ -4,14 +4,23 @@ import { withFormik, Form, Field } from "formik";
 import * as Yup from 'yup';
 
 function Login({touched, errors}) {
+
     return (
         //The Form component from Formik takes care of the change-handling and state management for the form
         <Form> 
          <h3>Admin Login</h3> 
-        <Field type="email" name="email" placeholder="Email" />
-        {touched.username && errors.username && (
-          <p className="error">{errors.username}</p>)}
-        <Field type="password" name="password" placeholder="Password" />
+        <Field 
+            type="username" 
+            name="username" 
+            placeholder="username"
+         />
+        {touched.username && errors.username && ( <p className="error">{errors.username}</p>)}
+
+        <Field 
+            type="password" 
+            name="password" 
+            placeholder="Password" 
+        />
         {touched.password && errors.password && <p className="error">{errors.password}</p>}
         
          <button type="submit">Login</button>
@@ -21,20 +30,22 @@ function Login({touched, errors}) {
 
 //Wiring the form up with Formik below. We do this by wrapping our login function inside the higher-order Formik component useing withFormik
 const FormikLogin = withFormik({
-    mapPropsToValues({ email, password }) {
+    mapPropsToValues({ username, password }) {
       return {
-        email: email || "",
+        username: username || "",
         password: password || ""
       };
     },
     validationSchema: Yup.object().shape({
-      username: Yup.string().required("Required Field"),
-      password: Yup.string().required("Required Field")
+      username: Yup.string()
+            .required("Required Field"),
+      password: Yup.string()
+            .required("Required Field")
     }),
     //You can use this to see the values
     handleSubmit(values, { props, resetForm }) {
         // console.log(values)
-      axios
+     axios
         .post("https://jspencer-be.herokuapp.com/auth/login", values)
         .then(res => {
           console.log(res.data)
