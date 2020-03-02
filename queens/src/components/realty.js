ad09d41af0f5009d590f80cff19993dabb1b7b96
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-01 17:02:31 -0800

    Imported RealtyCard into Realty.j

--- queens/src/components/realty.js
+++ queens/src/components/realty.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import RealtyCard from './RealtyCard';
+
 
 function Realty() {
     const [house, setHouse] = useState();
