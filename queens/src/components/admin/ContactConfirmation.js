81c206c7103ee4e140548eba488c6feb8f517747
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-10 17:11:43 -0700

    Created ContactConfirmation compoenent a user will receive after submitting a message

---
+++ queens/src/components/admin/ContactConfirmation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ContactConfirmation() {
+    return (
+        <div>
+            <h2>Thank You!</h2>
+            <p>I've received your message and will get back to you as soon as I can.</p>
+        </div>
+    )
+
+}
+
+export default ContactConfirmation;
