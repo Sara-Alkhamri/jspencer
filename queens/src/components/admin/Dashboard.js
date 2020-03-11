d4c3fda4c3897725bcccb5959e9f29bf40ffc3e6
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-10 20:23:06 -0700

    Added link to logout in admin dashboard

--- queens/src/components/admin/Dashboard.js
+++ queens/src/components/admin/Dashboard.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ContactMessages from './ContactMessage'
+import { Link } from 'react-router-dom';
 
 
 function Dashboard() {
+
+    function logout() {
+        localStorage.removeItem("token");
+      }
 
     return (
         <div className="site-section" id="contact-section">
@@ -10,6 +15,9 @@ function Dashboard() {
             <span className="sub-title mb-2 d-block">Hello admin!</span>
                 <h2 className="title text-primary mb-3">You've Got Mail!</h2>
                 <ContactMessages />
+                <Link to="/" onClick={event => logout(event)} className="home-nav-item">
+                    Logout
+                </Link>
             </div> 
     </div>
     )
