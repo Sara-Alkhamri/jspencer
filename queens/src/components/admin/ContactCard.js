30544e6042480578aee3e55a722311630a750d55
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-22 21:30:53 -0700

    Pushing unsaved files

--- queens/src/components/admin/ContactCard.js
+++ queens/src/components/admin/ContactCard.js
@@ -4,11 +4,13 @@ const ContactCard = props => {
     // console.log(props)
 
     return (
-        <div className="section-title text-center mb-5">
+        <div className="site-section" id="contact-section">
+        <div className="col-lg-5 h-100 jm-sticky-top">
             <h2>From: {props.firstName} {props.lastName}</h2>
             <p>Email: {props.email}</p>
             <p>Message: {props.message}</p>
             <button onClick={() => props.delete(props.id) } className="btn btn-primary btn-md">Delete</button>
+        </div>
         </div>
     )
 
