c45b9a3f43f604475aec99c95a9c120a6cebbef1
Sara Alkhamri <s.alkhamri@gmail.com>
2020-03-30 16:59:48 -0700

    Updates to Contact.js

--- queens/src/components/Contact.js
+++ queens/src/components/Contact.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { withFormik } from 'formik';
-import * as Yup from 'yup';
 import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup'
 
-const Contact = () => {
-
-    return (
+const ContactForm = () => {
+  return (
     <div className="site-section" id="contact-section">
       <div className="container">
-        <form method="post" className="contact-form">
-
-          <div className="section-title text-center mb-5">
+        <Form className="contact-form">
+        <div className="section-title text-center mb-5">
             <span className="sub-title mb-2 d-block">Get In Touch</span>
             <h2 className="title text-primary">Share Your Thoughts</h2>
           </div>
-
           <div className="row mb-4">
             <div className="col-md-6 mb-4 mb-md-0">
-              <input 
+              <Field 
               type="text" 
               name="firstname"
               className="form-control" 
@@ -25,7 +22,7 @@ const Contact = () => {
               />
             </div>
             <div className="col-md-6">
-              <input 
+              <Field 
               type="text" 
               name="lastName"
               className="form-control" 
@@ -36,7 +33,7 @@ const Contact = () => {
 
           <div className="row mb-4">
             <div className="col-12">
-              <input 
+              <Field 
               type="text" 
               name="email"
               className="form-control" 
@@ -55,62 +52,51 @@ const Contact = () => {
               </textarea>
             </div>
           </div>
-
           <div className="row">
             <div className="col-12">
               <button type="submit" className="btn btn-primary btn-md">Send Message</button>
             </div>
           </div>
-
-        </form>
+        </Form>
       </div>
     </div>
   )
 }
 
-const FormikContact = withFormik ({
-    mapPropsToValues({ firstName, lastName, email, message }) {
-        return {
-            firstName: firstName || "",
-            lastName: lastName || "",
-            email: email || "",
-            message: message || "",
-        };
-    },
+const FormikContactForm = withFormik({
+  mapPropsToValues({firstName, lastName, email, message}) {
+    return {
+      firstName: firstName || '',
+      lastName: lastName || '',
+      email: email || '',
+      message: message || ''
+    };
+  },
 
-    validationSchema: Yup.object().shape({
-        firstName: Yup.string()
-        .required("First name is required"),
-      
-        lastName: Yup.string()
-        .required("Last name is required"),
+  validationSchema: Yup.object().shape({
+    firstName: Yup.string()
+      .required('Required Field'),
+      lastName: Yup.string()
+      .required('Required Field'),
+      email: Yup.string()
+      .required('Required Field'),
+      password: Yup.string()
+      .required()
+  }),
 
-        email: Yup.string()
-        .required("Email is required"),
+  handleSubmit(values, {props, setStatus, resetForm}) {
+    //console.log(values)
+    axios
+    .post('https://jspencer-be.herokuapp.com/contact/submit', values)
+    .then(res => {
+        //console.log(res.data);
+        setStatus(res.data);
+        props.history.push('/contact-confirmation');
+        resetForm();
+    })
+    .catch(error => console.log(error.response));
+}
 
-        message: Yup.string()
-        .required("Message is required"),
+})(ContactForm)
 
-    }),
-
-    //Come back to this when it's time for DB work
-    handleSubmit(values, {props, resetForm}) {
-      console.log(props)
-        axios
-            .post("https://jspencer-be.herokuapp.com/contact/submit", values)
-            .then(res => {
-              console.log(res.data)
-                // console.log("handelSubmit: then: response: ", response);
-                // setStatus(res.data);
-                props.history.push("/contact-confirmation");
-                resetForm();
-            })
-            .catch (error => console.log("handelSubmit: then: error: ", error))
-    }    
-
-    
-
-})(Contact);
-
-
-export default FormikContact;
+export default FormikContactForm;
