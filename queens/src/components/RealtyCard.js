c3be4a7742c1be05648d5f7a0fa935fac445e133
Sara Alkhamri <s.alkhamri@gmail.com>
2020-04-03 16:08:28 -0700

    Added add listing button to RealtyCard.js

--- queens/src/components/RealtyCard.js
+++ queens/src/components/RealtyCard.js
@@ -5,6 +5,7 @@ const RealtyCard = (props) => {
     return (
             <div key={props.id} id={props.id}>
                 <img className="img-fluid" alt="houses" src={props.image}/>
+                <button onClick={() => props.post(props.image) } className="btn btn-primary btn-md">Add Listing</button>
             </div>
 
                       
